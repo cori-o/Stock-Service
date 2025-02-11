fe98353eb0da556b8be53f23f91952f2a668e6c7
Finger-cori <badzoo@naver.com>
2025-02-11 16:46:11 +0900

    Update store_convlog.py
    edit path

--- store_convlog.py
+++ store_convlog.py
@@ -58,9 +58,9 @@ def main(args):
 
 if __name__ == '__main__':
     cli_parser = argparse.ArgumentParser()
-    cli_parser.add_argument('--data_path', type=str, default='data/')
+    cli_parser.add_argument('--data_path', type=str, default='./data/')
     cli_parser.add_argument('--file_name', type=str, default='conv_log-0705-0902.xlsx')
-    cli_parser.add_argument('--config_path', type=str, default='config/')
+    cli_parser.add_argument('--config_path', type=str, default='./config/')
     cli_parser.add_argument('--task_name', type=str, default='cls')
     cli_parser.add_argument('--process', type=str, default='daily')
     cli_parser.add_argument('--query', type=str, default=None)
